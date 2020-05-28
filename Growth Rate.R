06841508881d8d79d754f891c016f3d6f282a5a0
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2020-05-28 14:35:53 -0500

    More Growth
    Using Growth Data

--- Growth Rate.R
+++ Growth Rate.R
@@ -32,3 +32,34 @@ Gr<-(b*(Temp-Tmin))^2/2.303 #growth rate
 ConInc<-(Gr*Time)-(.35*Time)
 NewCont<-log(CurrentCon)+ConInc
 exp(NewCont)
+
+
+#New Try
+setwd("C:/Users/reyes/Documents/GitHub/Share-Table-QMRA")
+
+#Inputs: 
+
+RelativeLag<-0 #we have to reset this after every iteration. 
+
+Time_ST<-6
+Time_StorageST<-.2
+Time_Selection<-.2
+Time_Consumption<-.5
+
+Temp_ST<-25
+Temp_StorageST<-4
+Temp_Selection<-25
+Temp_Consumption<-25
+
+
+Growth_Data<-read.csv("Growth Table.csv") 
+
+GrowthModel<-Func_seach_Data4(Growth_Data,Growth_Data$Temperature,Temp_ST,1)
+
+Growth_rate<-GrowthModel$Growth.Rate
+LagLength<-GrowthModel$Lag.Phase
+Nmax<-GrowthModel$Nmax
+
+RelativeLag<-Time_ST/LagLength + RelativeLag
+
+
