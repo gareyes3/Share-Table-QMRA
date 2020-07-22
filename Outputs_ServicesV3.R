f02c5c4101356b29cbde705b0f441902b8c207ff
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2020-07-22 11:56:53 -0500

    new growht functions

---
+++ Outputs_ServicesV3.R
@@ -0,0 +1,285 @@
+
+# Outputs creation on Individual service data frames -----------------------------------------------------------------
+
+Fr_DF <- paste("Fr_Data.Frame_D", k,"_S",j, sep = "")
+assign(Fr_DF, Fr_Data.Frame)
+
+Pss_DF <- paste("Pss_Data.Frame_D", k,"_S",j, sep = "")
+assign(Pss_DF, Pss_Data.Frame)
+
+Pre_DF <- paste("Pre_Data.Frame_D", k,"_S",j, sep = "")
+assign(Pre_DF, Pre_Data.Frame)
+
+
+# VECTORS -----------------------------------------------------------------
+
+Fr_Vector_Sh <- paste("V_Shared_Fr_M", j, sep = "")
+assign(Fr_Vector_Sh, V_Shared_Fr)
+
+Pss_Vector_Sh <- paste("V_Shared_Pss_M", j, sep = "")
+assign(Pss_Vector_Sh, V_Shared_Pss)
+
+Pre_Vector_Sh <- paste("V_Shared_Pre_M", j, sep = "")
+assign(Pre_Vector_Sh, V_Shared_Pre)
+
+
+
+# OUTPUTS FOR MEALS -----------------------------------------------------
+
+
+# FRUIT -------------------------------------------------------------------
+
+
+#Fruit that stayed in share table. 
+Left_ST_Fr<-Fr_Data.Frame[which(Fr_Data.Frame$Location == "Shared"),]
+#Fruit that stayed in Selection table
+Left_Selection_Fr<-Fr_Data.Frame[which(Fr_Data.Frame$Location == "Selection Table"),]
+#Consumed Fr day 1, for exposure assesment
+Consumed_Fr<-Fr_Data.Frame[which(Fr_Data.Frame$Location == "Consumed"),]
+#Amounts left of fruit. 
+No_Left_ST_Fr<-nrow(Left_ST_Fr)
+No_Left_Selection_Fr<-nrow(Left_Selection_Fr)
+
+
+# Adding Time Between Services Fruit --------------------------------------------
+
+if(j>0 && j<=(Service_No-1)){
+    #selection
+  Left_Selection_Fr$TotTime<-Func_Adding_Time(Left_Selection_Fr$TotTime, Time_Service)
+    #Share Table
+  Left_ST_Fr$TotTime<-Func_Adding_Time(Left_ST_Fr$TotTime, Time_Service)
+}
+
+
+
+# Adding Growth Between Every Service  Fruit-------------------------------------
+
+if(j>0 && j<=(Service_No-1)){
+  if(E_coli==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_Selection_Fr,Time_Service) #Using function on left over items
+    Left_Selection_Fr<-DF
+    #Share Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_ST_Fr,Time_Service) #using function on left over St items
+    Left_ST_Fr<-DF
+  }
+  if(salmonella==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_Selection_Fr,Time_Service) #Using function on left over items
+    Left_Selection_Fr<-DF
+    #Share Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_ST_Fr,Time_Service) #using function on left over St items
+    Left_ST_Fr<-DF
+  }
+  if(norovirus==1 && Growth ==1){
+    #selection table items
+    Func_Growth_Sto_Norovirus("refrigerated", Left_Selection_Fr, Time_Service) #using function on left over items
+    #Share table items
+    Func_Growth_Sto_Norovirus("refrigerated", Left_Selection_Fr, Time_Service) #Share table left over items
+  }
+}
+
+# Adding Turnaround Growth Fruit ------------------------------------------------
+
+if(j>0 && j<=(Service_No-1)){
+  if(E_coli==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_Selection_Fr, Time_Turnaround) #Using function on left over items
+    Left_Selection_Fr<-DF
+    #Share Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_ST_Fr, Time_Turnaround) #using function on left over St items
+    Left_ST_Fr<-DF
+  }
+  if(salmonella==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_Selection_Fr, Time_Turnaround) #Using function on left over items
+    Left_Selection_Fr<-DF
+    #Share Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_ST_Fr, Time_Turnaround) #using function on left over St items
+    Left_ST_Fr<-DF
+  }
+  if(norovirus==1 && Growth ==1){
+    #selection table items
+    Func_Growth_Sto_Norovirus("refrigerated", Left_Selection_Fr, Time_Turnaround) #using function on left over items
+    #Share table items
+    Func_Growth_Sto_Norovirus("refrigerated", Left_Selection_Fr, Time_Turnaround) #Share table left over items
+  }
+}
+
+
+# Washing Log Reduction ---------------------------------------------------
+
+if(Wash_Between_Services ==1){
+
+#Washing selection Items
+  if(Wash_Selection_YN_Fr==1){
+    Left_Selection_Fr$Contamination<-Func_Logred(Left_Selection_Fr$Contamination,Reduction_wash)
+  }
+  
+  #washing share table items
+  if(Wash_ST_YN_Fr==1){
+    Left_ST_Fr$Contamination<-Func_Logred(Left_ST_Fr$Contamination,Reduction_wash)
+  }
+}
+
+
+
+# PSS ---------------------------------------------------------------------
+
+
+#Pss that stayed in share table. 
+Left_ST_Pss<-Pss_Data.Frame[which(Pss_Data.Frame$Location == "Shared"),]
+#Pss that stayed in Selection table
+Left_Selection_Pss<-Pss_Data.Frame[which(Pss_Data.Frame$Location == "Selection Table"),]
+#Consumed Pss day 1, for exposure assesment
+Consumed_Pss<-Pss_Data.Frame[which(Pss_Data.Frame$Location == "Consumed"),]
+#Amounts left of Pss. 
+No_Left_ST_Pss<-nrow(Left_ST_Pss)
+No_Left_Selection_Pss<-nrow(Left_Selection_Pss)
+
+
+# Adding time between services Pss ----------------------------------------
+
+if(j>0 && j<=(Service_No-1)){
+  #selection
+  Left_Selection_Pss$TotTime<-Func_Adding_Time(Left_Selection_Pss$TotTime, Time_Service)
+  #Share Table
+  Left_ST_Pss$TotTime<-Func_Adding_Time(Left_ST_Pss$TotTime, Time_Service)
+}
+
+
+# Adding Growth Between Every Service  Pss-------------------------------------
+
+if(j>0 && j<=(Service_No-1)){
+  if(E_coli==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_Selection_Pss,Time_Service) #Using function on left over items
+    Left_Selection_Pss<-DF
+    #Share Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_ST_Pss,Time_Service) #using function on left over St items
+    Left_ST_Pss<-DF
+  }
+  if(salmonella==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_Selection_Pss,Time_Service) #Using function on left over items
+    Left_Selection_Pss<-DF
+    #Share Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_ST_Pss,Time_Service) #using function on left over St items
+    Left_ST_Pss<-DF
+  }
+  if(norovirus==1 && Growth ==1){
+    #selection table items
+    Func_Growth_Sto_Norovirus_Plastic("room temp", Left_Selection_Pss, Time_Service) #using function on left over items
+    #Share table items
+    Func_Growth_Sto_Norovirus_Plastic("room temp", Left_Selection_Pss, Time_Service) #Share table left over items
+  }
+}
+
+# Adding Turnaround Growth Pss ------------------------------------------------
+
+if(j>0 && j<=(Service_No-1)){
+  if(E_coli==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_Selection_Pss, Time_Turnaround) #Using function on left over items
+    Left_Selection_Pss<-DF
+    #Share Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_ST_Pss, Time_Turnaround) #using function on left over St items
+    Left_ST_Pss<-DF
+  }
+  if(salmonella==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_Selection_Pss, Time_Turnaround) #Using function on left over items
+    Left_Selection_Pss<-DF
+    #Share Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_ST_Pss, Time_Turnaround) #using function on left over St items
+    Left_ST_Pss<-DF
+  }
+  if(norovirus==1 && Growth ==1){
+    #selection table items
+    Func_Growth_Sto_Norovirus_Plastic("room temp", Left_Selection_Pss, Time_Turnaround) #using function on left over items
+    #Share table items
+    Func_Growth_Sto_Norovirus_Plastic("room temp", Left_Selection_Pss, Time_Turnaround) #Share table left over items
+  }
+}
+
+
+# PRE ---------------------------------------------------------------------
+
+
+
+#Pre that stayed in share table. 
+Left_ST_Pre<-Pre_Data.Frame[which(Pre_Data.Frame$Location == "Shared"),]
+#Pre that stayed in Selection table
+Left_Selection_Pre<-Pre_Data.Frame[which(Pre_Data.Frame$Location == "Selection Table"),]
+#Consumed Pre day 1, for exposure assesment
+Consumed_Pre<-Pre_Data.Frame[which(Pre_Data.Frame$Location == "Consumed"),]
+#Amounts left of Pre 
+No_Left_ST_Pre<-nrow(Left_ST_Pre)
+No_Left_Selection_Pre<-nrow(Left_Selection_Pre)
+
+# Adding time between services Pre ----------------------------------------
+if(j>0 && j<=(Service_No-1)){
+  #selection
+  Left_Selection_Pre$TotTime<-Func_Adding_Time(Left_Selection_Pre$TotTime, Time_Service)
+  #Share Table
+  Left_ST_Pre$TotTime<-Func_Adding_Time(Left_ST_Pre$TotTime, Time_Service)
+}
+
+
+# Adding Growth Between Every Service  Pre-------------------------------------
+
+if(j>0 && j<=(Service_No-1)){
+  if(E_coli==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_Selection_Pre,Time_Service) #Using function on left over items
+    Left_Selection_Pre<-DF
+    #Share Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_ST_Pre,Time_Service) #using function on left over St items
+    Left_ST_Pre<-DF
+  }
+  if(salmonella==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_Selection_Pre,Time_Service) #Using function on left over items
+    Left_Selection_Pre<-DF
+    #Share Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_ST_Pre,Time_Service) #using function on left over St items
+    Left_ST_Pre<-DF
+  }
+  if(norovirus==1 && Growth ==1){
+    #selection table items
+    Func_Growth_Sto_Norovirus_Plastic("room temp", Left_Selection_Pre, Time_Service) #using function on left over items
+    #Share table items
+    Func_Growth_Sto_Norovirus_Plastic("room temp", Left_Selection_Pre, Time_Service) #Share table left over items
+  }
+}
+
+# Adding Turnaround Growth Pre ------------------------------------------------
+
+if(j>0 && j<=(Service_No-1)){
+  if(E_coli==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_Selection_Pre, Time_Turnaround) #Using function on left over items
+    Left_Selection_Pre<-DF
+    #Share Table Items
+    Func_Growth_Sto_Ecoli("room temp", Left_ST_Pre, Time_Turnaround) #using function on left over St items
+    Left_ST_Pre<-DF
+  }
+  if(salmonella==1 && Growth ==1){
+    #Selection Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_Selection_Pre, Time_Turnaround) #Using function on left over items
+    Left_Selection_Pre<-DF
+    #Share Table Items
+    Func_Growth_Sto_Salmonella("room temp", Left_ST_Pre, Time_Turnaround) #using function on left over St items
+    Left_ST_Pre<-DF
+  }
+  if(norovirus==1 && Growth ==1){
+    #selection table items
+    Func_Growth_Sto_Norovirus_Plastic("room temp", Left_Selection_Pre, Time_Turnaround) #using function on left over items
+    #Share table items
+    Func_Growth_Sto_Norovirus_Plastic("room temp", Left_Selection_Pre, Time_Turnaround) #Share table left over items
+  }
+}
+
+
+
+
