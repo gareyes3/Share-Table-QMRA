5ea2f5662359c9b7bda476177ed99571be1a5ade
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2020-08-20 15:43:00 -0500

    update

--- R/ST_RandomInputs.R
+++ R/ST_RandomInputs.R
@@ -38,8 +38,7 @@
     mass_feces_hands<- rbetagen(1,4.57,2.55,-8.00,-1.00) #log(g/hands)
     HU_NV_in_Feces<- rlnormTrunc(1,6.65,2.06,0.0,10.98) #log HuNov CG/ g
     Genomic_copies_per_PFU<-rnormTrunc(1,3.65,.98,2.00,5.40)
-    Personal_Contamination<-((10^mass_feces_hands) * (10^HU_NV_in_Feces))/(10^Genomic_copies_per_PFU) #PFU/Hand
-    IC_Student<- Personal_Contamination #CFU/Hand 
+    IC_Student<-((10^mass_feces_hands) * (10^HU_NV_in_Feces))/(10^Genomic_copies_per_PFU) #PFU/Hand
   }
 
 
