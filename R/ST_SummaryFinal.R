43607277147e82f55ffc3abd817ae1ec8f2721e7
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2020-09-04 17:37:34 -0500

    sens

--- R/ST_SummaryFinal.R
+++ R/ST_SummaryFinal.R
@@ -5,3 +5,9 @@ class(AFr_Summary_DF$Cont5th)
 
 Histogram_Fr_Total<-Exposure_Plot_Function4(Consumed = AFr_Summary_DF, Title = "One", xlab = "X", ylab = "3")
 Histogram_Fr_Total
+
+ggplot(AFr_Summary_DF,aes(Iteration.N, MedianCont)) + 
+  geom_ribbon(aes(ymin = Cont5th, ymax = Cont95th), fill = "springgreen1")+  
+  geom_line(aes(Iteration.N, Cont5th), color = "grey30", size = 0.1) + 
+  geom_line(aes(Iteration.N, Cont95th), color = "grey30", size = 0.1) +   
+  geom_line(color = "firebrick", size = 1)
