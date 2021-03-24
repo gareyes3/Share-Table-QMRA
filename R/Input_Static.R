7399f63c541d35ffc7833edeb6fe6c84dd5d7b6d
gareyes3 <64931750+gareyes3@users.noreply.github.com>
2021-03-24 08:16:42 -0500

    updating documents

--- R/Input_Static.R
+++ R/Input_Static.R
@@ -27,7 +27,7 @@
 
   #Weeks
   
-  Sens_Iterations<-10
+  Sens_Iterations<-1
  
 # Sevice Line Information -------------------------------------------------
   
--- R/NSA_Options_For_Inputs.R
+++ R/NSA_Options_For_Inputs.R
@@ -1,59 +1,59 @@
 #NRSA Options
 
 if(TreatmentNo==1){
-  source("NSA_Input_Random_T1.R")
+  source("NSA_Source/NSA_Input_Random_T1.R")
 } else if (TreatmentNo==2){
-  source("NSA_Input_Random_T2.R")
+  source("NSA_Source/NSA_Input_Random_T2.R")
 } else if (TreatmentNo==3){
-  source("NSA_Input_Random_T3.R")
+  source("NSA_Source/NSA_Input_Random_T3.R")
 } else if (TreatmentNo==4){
-  source("NSA_Input_Random_T4.R")
+  source("NSA_Source/NSA_Input_Random_T4.R")
 } else if (TreatmentNo==5){
-  source("NSA_Input_Random_T5.R")
+  source("NSA_Source/NSA_Input_Random_T5.R")
 } else if (TreatmentNo==6){
-  source("NSA_Input_Random_T6.R")
+  source("NSA_Source/NSA_Input_Random_T6.R")
 }else if (TreatmentNo==7){
-  source("NSA_Input_Random_T7.R")
+  source("NSA_Source/NSA_Input_Random_T7.R")
 }else if (TreatmentNo==8){
-  source("NSA_Input_Random_T8.R")
+  source("NSA_Source/NSA_Input_Random_T8.R")
 }else if (TreatmentNo==9){
-  source("NSA_Input_Random_T9.R")
+  source("NSA_Source/NSA_Input_Random_T9.R")
 }else if (TreatmentNo==10){
-  source("NSA_Input_Random_T10.R")
+  source("NSA_Source/NSA_Input_Random_T10.R")
 }else if (TreatmentNo==11){
-  source("NSA_Input_Random_T11.R")
+  source("NSA_Source/NSA_Input_Random_T11.R")
 }else if (TreatmentNo==12){
-  source("NSA_Input_Random_T12.R")
+  source("NSA_Source/NSA_Input_Random_T12.R")
 }else if (TreatmentNo==16){
-  source("NSA_Input_Random_T16.R")
+  source("NSA_Source/NSA_Input_Random_T16.R")
 }else if (TreatmentNo==17){
-  source("NSA_Input_Random_T17.R")
+  source("NSA_Source/NSA_Input_Random_T17.R")
 }else if (TreatmentNo==18){
-  source("NSA_Input_Random_T18.R")
+  source("NSA_Source/NSA_Input_Random_T18.R")
 }else if (TreatmentNo==19){
-  source("NSA_Input_Random_T19.R")
+  source("NSA_Source/NSA_Input_Random_T19.R")
 }else if (TreatmentNo==20){
-  source("NSA_Input_Random_T20.R")
+  source("NSA_Source/NSA_Input_Random_T20.R")
 }else if (TreatmentNo==21){
-  source("NSA_Input_Random_T21.R")
+  source("NSA_Source/NSA_Input_Random_T21.R")
 }else if (TreatmentNo==22){
-  source("NSA_Input_Random_T22.R")
+  source("NSA_Source/NSA_Input_Random_T22.R")
 }else if (TreatmentNo==23){
-  source("NSA_Input_Random_T23.R")
+  source("NSA_Source/NSA_Input_Random_T23.R")
 }else if (TreatmentNo==24){
-  source("NSA_Input_Random_T24.R")
+  source("NSA_Source/NSA_Input_Random_T24.R")
 }else if (TreatmentNo==25){
-  source("NSA_Input_Random_T25.R")
+  source("NSA_Source/NSA_Input_Random_T25.R")
 }else if (TreatmentNo==26){
-  source("NSA_Input_Random_T26.R")
+  source("NSA_Source/NSA_Input_Random_T26.R")
 }else if (TreatmentNo==27){
-  source("NSA_Input_Random_T27.R")
+  source("NSA_Source/NSA_Input_Random_T27.R")
 }else if (TreatmentNo==28){
-  source("NSA_Input_Random_T28.R")
+  source("NSA_Source/NSA_Input_Random_T28.R")
 }else if (TreatmentNo==29){
-  source("NSA_Input_Random_T29.R")
+  source("NSA_Source/NSA_Input_Random_T29.R")
 }else if (TreatmentNo==30){
-  source("NSA_Input_Random_T30.R")
+  source("NSA_Source/NSA_Input_Random_T30.R")
 }else{
   source("Input_Random.R")
 }
